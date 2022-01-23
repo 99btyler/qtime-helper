8ae038338f1b6ac8b5c9346aca9a92ca64180e71
99btyler <99tylerberinger@gmail.com>
2022-01-23 00:52:07 -0500

    Added print statements

--- src/main.py
+++ src/main.py
@@ -7,18 +7,22 @@ def ask(question, requires_number=False):
 	return int(answer) if requires_number else answer
 
 
+print("\n---- qtime-helper ----\n")
+
 column_name = ask("column_name")
-row_first = ask("row_first", True)
+row_first = ask("\nrow_first", True)
 row_last = ask("row_last", True)
-goods = ask("goods(separated by comma)")
+goods = ask("\ngoods(separated by comma)")
 
 string_all = ""
 for i in range(row_first, row_last+1):
 	string_all += f"{column_name}{i}+"
-print(string_all[:-1])
+print(f"\n{string_all[:-1]}")
 
 string_goods = ""
 for good in [good.strip() for good in goods.split(",")]:
 	string_goods += f"{column_name}{good}+"
-print(string_goods[:-1])
+print(f"{string_goods[:-1]}")
+
+print("\n----------------------\n")
 
